a8f360e55e43b2eda3e6ba39bc852305d762c8ae
wendell00 <wendellborges2003@gmail.com>
2023-01-27 19:52:21 -0300

    Arquivos iniciais

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-[#464646] w-screen h-screen">
+      
+    </div>
+  )
+}
+
+export default App
