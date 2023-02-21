b63ed93c549efab70c650fac86ea03a5fd6365f2
wendell00 <wendellborges2003@gmail.com>
2023-02-21 19:46:39 -0300

    adicionando icon do github

--- src/layouts/LayoutDefault/index.jsx
+++ src/layouts/LayoutDefault/index.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from 'react-router-dom'
+import Git from '/assets/gitIcon.svg'
 
 export function LayoutDefault() {
   return (
     <div className='w-full min-h-[650px] h-screen flex justify-center items-center bg-[#cfe2fe]'>
+      <a href="https://github.com/Wendell00" target="_blank">
+        <img src={Git} className="absolute top-[1%] right-[1%] z-50 w-[40px]"></img>
+      </a>
       <main className='h-full w-[960px] md:h-[580px] bg-[#f0f6ff] md:rounded-2xl shadow-2xl md:p-10'>
         <div className='relative w-full h-full flex'>
           <Outlet />
